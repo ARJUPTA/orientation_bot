915702b9ba53af688dee052176d3e18097658aac
Aishwary <aishwary.saxena.min19@itbhu.ac.in>
2020-12-11 16:11:46 +0530

    Added memes, gif, image and google searches

--- api/functions/memes/index.js
+++ api/functions/memes/index.js
@@ -1,1 +1,23 @@
-/* Add functions for meme command */
+import axios from "axios";
+import { Extra } from "telegraf";
+
+export const getMemeCommands = (ctx) => {
+    const commands = "meymeyy\n\n/randomMeme\n/wholesomeMeme\n/historyMeme\n/memeEconomy\n/animeMeme\n/toorealMeme\n/indianMeme\n/cheems\n/herapheri";
+    ctx.reply(commands);
+}
+
+export const getMeme = (ctx, memeCategory) => {
+    console.log(ctx.from.first_name, ": /", memeCategory)
+    var url = "https://meme-api.herokuapp.com/gimme/" + memeCategory + "/"
+    console.log(url)
+    axios.get(url).then(function (response) {
+        console.log(response)
+        var photo = response.data.url.toString();
+        var post = response.data.postLink.toString();
+        var caption = response.data.title.toString() + " (" + post + ")";
+      ctx.replyWithPhoto(photo, Extra.caption(caption).markdown())
+    }).catch(function (response) {
+        console.log(response)
+        ctx.reply(`Some error occured while fetching the meme, please try again later!`);
+    });
+  }
--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-import { Telegraf, Extra } from "telegraf";
+import { Telegraf } from "telegraf";
 import dotenv from 'dotenv';
 import { getCat, getDog } from './functions/animals';
 import { lingo } from './functions/lingo';
 import { greet, stickerPhotoReply } from "./functions/utils";
+import { getMeme, getMemeCommands } from "./functions/memes";
+import { googleSearch, imageSearch } from "./functions/google-search";
+import { getGif } from "./functions/giphy";
+import { wordSearch } from "./functions/dictionary";
 
 dotenv.config();
 const bot = new Telegraf(process.env.TOKEN);
@@ -14,20 +18,39 @@ bot.start((ctx)=>{
 });
 
 bot.catch((err, ctx) => {
-    console.log('Error, ', ctx.from.first_name)
+    console.log('Error, ', ctx.from.first_name, " - ", err)
+    console.log(err.message)
     const errMsg = "Did not quite get what you said, please contact @aishwary023 for the issue!";
     ctx.reply(errMsg);
 });
 
 bot.help((ctx) => {
     console.log(ctx.from.first_name,": /help")
-    const helpMsg ="/lingo for lingo \n\n/dog - For a random dog photo\n\n/cat - For a random cat photo\n\nWhy dogs and cats? Because I am a wholesome bot!\n\n/help - For all available function\nGot any other query? Contact these people anytime: ";
+    const helpMsg ="/dict or /dictionary for using dictionary\n\n/lingo for lingo \n\n/dog - For a random dog photo\n\n/cat - For a random cat photo\n\nWhy dogs and cats? Because I am a wholesome bot!\n\n/help - For all available function\nGot any other query? Contact these people anytime: ";
     ctx.replyWithMarkdown(helpMsg);
 });
 
 bot.command('dog', (ctx) => getDog(ctx));
 bot.command('cat', (ctx) => getCat(ctx));
 bot.command('lingo', (ctx) => lingo(ctx));
+bot.command('dict', (ctx) => wordSearch(ctx));
+bot.command('dictionary', (ctx) => wordSearch(ctx));
+
+bot.command('meme', (ctx) => getMemeCommands(ctx));
+bot.command('randomMeme', (ctx)=> getMeme(ctx, 'memes'));
+bot.command('wholesomeMeme', (ctx)=> getMeme(ctx, 'wholesomememes'));
+bot.command('indianMeme', (ctx)=> getMeme(ctx, 'IndianMeyMeys'));
+bot.command('memeEconomy', (ctx)=> getMeme(ctx, 'MemeEconomy'));
+bot.command('historyMeme', (ctx)=> getMeme(ctx, 'HistoryMemes'));
+bot.command('animeMeme', (ctx) => getMeme(ctx, 'Animemes'));
+bot.command('toorealMeme', (ctx) => getMeme(ctx, '2meirl4meirl'));
+bot.command('cheems', (ctx)=> getMeme(ctx, 'dogelore'));
+bot.command('herapheri', (ctx)=> getMeme(ctx, 'HeraPheriMemes'));
+
+bot.command('google', (ctx) => googleSearch(ctx));
+bot.command('image', (ctx) => imageSearch(ctx));
+
+bot.command('gif', (ctx) => getGif(ctx))
 
 bot.on('message', (ctx) => greet(ctx));
 bot.on(['sticker', 'photo'], (ctx) => stickerPhotoReply(ctx));
